b56379d2fcbf85bd5e7c00a8af670df34f0b5e17
Shubhradeep Maity <shubhradeepmaity3@gmail.com>
2024-09-26 00:36:00 +0530

    Compare Password

--- models/user.js
+++ models/user.js
@@ -47,5 +47,35 @@ default:false
      default: Date.now() },
 });
 
+
+// one admin =>
+   // This logic is implemented in the pre('save') middleware, so it’s handled every time a new user is created. Additionally, this approach maintains system integrity by preventing race conditions, even if multiple requests are made simultaneously.  
+
+
+// Pre-save hook to hash password and check for existing admin
+// userSchema.pre('save', async function (next) {
+//    const person = this;
+
+//    // If the user is being created with the 'admin' role, check if an admin already exists
+//    if (person.role === 'admin') {
+//        const existingAdmin = await mongoose.models.User.findOne({ role: 'admin' });
+//        if (existingAdmin) {
+//            const error = new Error('An admin already exists. Only one admin is allowed.');
+//            return next(error);
+//        }
+//    }
+// }
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+   try{
+       // Use bcrypt to compare the provided password with the hashed password
+       const isMatch = await bcrypt.compare(candidatePassword, this.password);
+       return isMatch;
+   }catch(err){
+       throw err;
+   }
+}
+
+
 const User = mongoose.model('User',userSchema );
 module.exports=User;
